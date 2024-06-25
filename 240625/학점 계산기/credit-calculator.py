14a48cfc645a70f059814e50efb50fcc74723231
Yerin99 <63496777+Yerin99@users.noreply.github.com>
2024-06-25 13:29:04 +0900

    Title: 학점 계산기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240625/학점 계산기/credit-calculator.py
@@ -0,0 +1,11 @@
+n = int(input())
+grades = list(map(float, input().split()))
+gpa = round(sum(grades) / n, 1)
+print(gpa)
+
+if gpa > 4.0:
+    print("Perfect")
+elif gpa > 3.0:
+    print("Good")
+else:
+    print("Poor")
